8da1151e27cf333885ef213ced4fde21ca4ab60d
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-01-29 11:52:47 +0100

    Disable Google Analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -24,7 +24,6 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    googleAnalyticsId: 'G-54901CB409', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus

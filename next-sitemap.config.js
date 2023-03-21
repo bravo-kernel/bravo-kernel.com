2643e0836e9f3a6b81bb6d57c5f2ad7c4186a2c9
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-03-21 10:08:27 +0100

    Replace pliny sitemap with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+const siteMetadata = require('/data/siteMetadata')
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: siteMetadata.siteUrl,
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  alternateRefs: [],
+}
